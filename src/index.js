113b7a00da08df54d4cc1d884c0a4d9a1b2913a2
Victor Sharamet <sharamet@gmail.com>
2019-03-24 22:51:44 +0300

    add: add implementation of the task in index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  let length = preferences.length
+  let count = 0
+  for (let i = 0; i < length; i++) { 
+      let a = preferences[i] - 1
+      if (a == i) continue
+      let b = preferences[a] - 1
+      if (a == b) continue
+      let c = preferences[b] - 1
+      if (c == b) continue
+      if (c == i) {
+          count++
+      }
+   }
+   return count / 3
+}
